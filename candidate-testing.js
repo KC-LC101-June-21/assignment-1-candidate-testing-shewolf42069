5ff4ede3046426ccfe6c42068332a40feb1e2be4
Lori Poje <85008400+shewolf42069@users.noreply.github.com>
2021-06-10 04:27:36 +0000

    assignment 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,56 +4,71 @@ const input = require('readline-sync');
 
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-//let greeting = "Welcome " + candidateName + "!";
-let question;
-let correctAnswer = "Sally Ride";
-let candidateAnswer = "";
-let questions;
-let correctAnswers = "";
-let candidateAnswers;
 
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+
+let question = [];
+let correctAnswer = [];
+let candidateAnswer = "";
+let questions = [
+  "Who was the first American woman in space? ", 
+   "True or false: 5 kilometer == 5000 meters? ", 
+   "(5 + 3)/2 * 10 = ? ", 
+   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+   "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
+let grade = 100;
 
 function askForName(candidateName) {
   // TODO 1.1b: Ask for candidate's name //
     candidateName = input.question("May I have your first name please? ");
-console.log();
-console.log("Welcome " + candidateName + "!");
-console.log();
+    console.log();
+    console.log("Welcome " + candidateName + "!");
+    console.log();
 }
+
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-      candidateAnswer = input.question("Who was the first American woman in space? ");
+
+for (let i = 0; i <= questions.length; i++) {
+
+      candidateAnswer = input.question(questions[i]);
+      candidateAnswers.push(candidateAnswer);
+
+//console.log(candidateAnswers);
+
+    if (candidateAnswer === correctAnswers[i]) {
+    console.log("Perfect!");
+    console.log();
+      }else {
+    console.log("I'm sorry, that is not correct");
+    //grade = grade - 20;
+    console.log();
+    }
+  } 
 }
 
 function gradeQuiz() {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-candidateAnswers = candidateAnswer;
-console.log();
-if (candidateAnswer === correctAnswer) {
-    console.log("Perfect!");
-}else {
-    console.log("I'm sorry, that is not correct");
-    console.log()
-}
 
-  let grade;
-  
+  //grade;
 
-  return grade;
+
+  //return grade;
 }
 
 function runProgram() {
- 
 
   // TODO 1.1c: Ask for candidate's name //
-   askForName();
- 
+
+  askForName();
   askQuestion();
 
   console.log(gradeQuiz(this.candidateAnswers));
+  
 }
 
 // Don't write any code below this line //
